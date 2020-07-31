778b5de87e660f6d972ee1c4293bbe466e140f62
archena <archena@users.noreply.github.com>
2020-07-31 13:59:12 +0100

    Layout work

--- dash/app.py
+++ dash/app.py
@@ -1,34 +1,72 @@
+from collections import OrderedDict
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import pandas as pd
+import numpy as np
 import plotly.graph_objs as go
 import plotly.express as px
 
-app = dash.Dash()
+external_stylesheets = [
+    "/assets/style.css"
+]
+
+app = dash.Dash(
+    external_stylesheets=external_stylesheets,
+    meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1.0"}
+    ],
+)
 
 pressure_data = pd.read_csv("../data/sample.csv", index_col=0, parse_dates=[0])
-#pressure_ts = px.line(pressure_data, se='Date', y='AAPL.High')
 
-#pressure_data_by_sensor = ...
+pressure_plots = []
+for sensor in [1, 3, 4]:
+    series = pressure_data[pressure_data.sensor == sensor]
+    scatter = go.Scatter(x = series.index,
+                         y = series.pressure,
+                         name = f"Sensor {sensor}",
+                         opacity = 0.4)
+    pressure_plots.append(scatter)
+    #pressure_plots.append(px.line(x=series.index, y=series.pressure, title=f"Sensor {sensor}"))
 
-s = go.Scatter(x=pressure_data.index,
-               y=pressure_data.pressure,
-               name = "Pressure timeseries",
-               line = dict(color = 'blue'),
-               opacity = 0.4)
-
-layout = dict(title='Pressure timeseries')
-
-fig = dict(data=[s], layout=layout)
-
-
-app.layout = html.Div([
-    dcc.Graph(
-        id='pressure-time-series',
-        figure=fig
+fig = dict(
+    data = pressure_plots,
+    layout = dict(
+        title="Pressure timeseries",
+        template = "plotly_dark"
     )
-])
+)
 
-if __name__ == '__main__':
+app.layout = html.Div(
+    [
+        html.Div(
+            id="header",
+            children=[
+                html.Div(
+                    [
+                        html.H3(
+                            "Wearable My Foot visualisation"
+                        )
+                    ],
+                    className="eight columns",
+                )
+            ],
+            className="row",
+        ),
+        html.Div(
+            [
+                dcc.Graph(
+                    id="pressure-time-series",
+                    figure=fig
+                )                
+            ],
+            className="row",
+        ),
+    ]
+)
+
+
+if __name__ == "__main__":
     app.run_server(debug=True)
